37f84a9607c8997e9fb2e2c1b6cbfa91794fd56f
itsme-jh <42227828+itsme-jh@users.noreply.github.com>
2022-05-07 16:56:08 +0800

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,9 @@
+export const fillDOM = (total: number, line: number, width: string) => {
+    // 接收  总数，每行个数，每个宽度
+    const n = line - (total % line);
+    const li = [];
+    for (let i = 0; i < n; i++) {
+        li.push(<li key={i} style={{ width: width }}></li>);
+    }
+    return li;
+};
